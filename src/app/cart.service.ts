7795aefbfb111fca826c1e49536cb220cbad7c86
rsotniko <rsotnik@gmail.com>
2022-08-15 13:11:38 +0200

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -9,4 +9,17 @@ export class CartService {
 
     items: Product[] = [];
   /* . . . */
+  addToCart(product: Product) {
+    this.items.push(product);
   }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+}
