81d0585cd2bf6abb06571fde2c0666caf7a1e828
rsotniko <rsotnik@gmail.com>
2022-08-17 10:36:03 +0200

    4 files modified, 4 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Product } from './products';
+import { HttpClient } from '@angular/common/http';
+
 
 
 @Injectable({
@@ -9,6 +11,11 @@ export class CartService {
 
     items: Product[] = [];
   /* . . . */
+  
+  constructor(
+    private http: HttpClient
+  ) {}
+
   addToCart(product: Product) {
     this.items.push(product);
   }
@@ -20,6 +27,10 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 
 }
